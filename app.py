b8333e9a80bde1824307cba75f519d862cf178e3
Tingting Wang <tingting.wang@Tingtings-MacBook-Pro.local>
2024-07-18 22:00:00 -0700

    first

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {name}!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
